e2d19d0a971fcebe53ef8813698d3d384d31b84b
Mastermind <mastermindx33@gmail.com>
2023-03-01 18:12:34 +0530

    log

--- JavaScript Algorithms and DataStructures/Intermediate Algorithm Scripting/SumAllNumbersInARange.js
+++ JavaScript Algorithms and DataStructures/Intermediate Algorithm Scripting/SumAllNumbersInARange.js
@@ -16,3 +16,25 @@ function sumAll(arr) {
   }
   
   sumAll([1, 4]);
+
+
+  // OR
+
+  function sumAll(arr) {
+    let total = 0;
+    let nums = [];
+    let lower = Math.min(...arr);
+    let upper = Math.max(...arr);
+  
+    for(let num = lower; num <= upper; num++ ){
+        nums.push(num);
+    }
+    // changed here in to of
+    for(let number of nums){
+      total += number;
+    }
+  
+    return total;
+  }
+  
+  sumAll([1, 4]);
